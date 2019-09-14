58ce89428d906f93f1803206f6457b7f897b6b9e
Arne Coucheron <arco68@gmail.com>
2019-09-15 00:07:46 +0200

    hertz: bluetooth: Remove BLE_PRIVACY_SPT
    Change-Id: Ic9b3282cd43a07779c31c932877295d1716f697b

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -13,10 +13,6 @@
 
 #define BTA_DM_COD {0x20, BTM_COD_MAJOR_AUDIO, BTM_COD_MINOR_SET_TOP_BOX}
 
-// Turn off BLE_PRIVACY_SPT.  Remote reconnect fails on
-// often if this is enabled.
-#define BLE_PRIVACY_SPT FALSE
-
 #define BTM_BLE_CONN_INT_MIN_DEF     0x18
 #define BTM_BLE_CONN_INT_MAX_DEF     0x28
 #define BTM_BLE_CONN_TIMEOUT_DEF     200
