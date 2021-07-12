c03ae34b78f5d41a80744e1fb2341e91f56d53f0
Jan Altensen <info@stricted.net>
2021-07-12 21:45:04 +0200

    [DNM] m5: Set fingerprint to deadpool
    * ATV SUW whitelists fingerprints/descriptions.
    
    Change-Id: Ied0cf87769be2a92f356bf24aecea422d62ff9f8

--- lineage_m5.mk
+++ lineage_m5.mk
@@ -18,6 +18,13 @@ $(call inherit-product, $(LOCAL_PATH)/device.mk)
 ## Device identifier. This must come after all inclusions
 PRODUCT_BRAND := bananapi
 PRODUCT_DEVICE := m5
+PRODUCT_GMS_CLIENTID_BASE := android-askey-tv
 PRODUCT_MANUFACTURER := bananapi
 PRODUCT_MODEL := BPI-M5
 PRODUCT_NAME := lineage_m5
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=adt3 \
+    PRIVATE_BUILD_DESC="adt3-user 11 RTT1.200909.003.A2 6832896 release-keys"
+
+BUILD_FINGERPRINT := ADT-3/adt3/adt3:11/RTT1.200909.003.A2/6832896:user/release-keys
