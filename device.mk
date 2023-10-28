2c21e0ce3a5a0a38a7f9c932bdbed150e5d11bc7
Vitaliy Tomin <highwaystar.ru@gmail.com>
2023-10-28 22:40:17 +0800

    hertz: Define DHD version

--- device.mk
+++ device.mk
@@ -18,6 +18,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init-files/init.amlogic.wifi_buildin.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.amlogic.wifi_buildin.rc
 
 ## Kernel Modules
+TARGET_DHD_VERSION := bcmdhd.100.10.545.x
+
 PRODUCT_PACKAGES += \
     dhd
 
